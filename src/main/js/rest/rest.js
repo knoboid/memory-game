32d3ef28cfb957ae8b559a851e6a3303ba203251
MJ Bingley <kn_b_y@yahoo.co.uk>
2019-12-11 13:42:44 +0000

    Export each rest method individually.

--- src/main/js/rest/rest.js
+++ src/main/js/rest/rest.js
@@ -1,69 +1,67 @@
 import client from '../client';
 
-const getSeeks = (cb) => {
+export const getSeeks = (cb) => {
     client({method: 'GET', path: '/api/seeks'}).done((response, error) => {
         cb(response, error);
     });	
 }
 
-const getSeek = (cb, id) => {
+export const getSeek = (cb, id) => {
     client({method: 'GET', path: '/api/seek', params: {id}}).done((response, error) => {
         cb(response, error);
     });	
 }
 
-const getSeeksBySeekerId = (cb, seekerId) => {
+export const getSeeksBySeekerId = (cb, seekerId) => {
     client({method: 'GET', path: '/api/seeksbyseekerid', params: {seekerId}}).done((response, error) => {
         cb(response, error);
     });	
 }
 
-const deleteSeeksBySeekerId = (cb, seekerId) => {
+export const deleteSeeksBySeekerId = (cb, seekerId) => {
     client({method: 'DELETE', path: '/api/seeksbyseekerid', params: {seekerId}}).done((response, error) => {
         cb(response, error);
     });	
 }
 
-const postSeek = (cb, playerId, cardPairCount) => {
+export const postSeek = (cb, playerId, cardPairCount) => {
     client({method: 'POST', path: '/api/seek', params: {playerId, cardPairCount}}).done((response, error) => {		
         cb(response, error);
     });
 }
 
-const postAccept = (cb, seekId, playerId) => {
+export const postAccept = (cb, seekId, playerId) => {
     client({method: 'POST', path: '/api/accept', params: {seekId, playerId}}).done((response, error) => {
         cb(response, error);
     });
 }
 
-const getGame = (cb, id) => {
+export const getGame = (cb, id) => {
     client({method: 'GET', path: '/api/game', params: {id}}).done((response, error) => {
         cb(response, error);
     });	
 }
 
-const postMove = (cb, gameId, playerId, cardIndex) => {
+export const postMove = (cb, gameId, playerId, cardIndex) => {
     client({method: 'POST', path: '/api/move', params: {gameId, playerId, cardIndex}}).done((response, error) => {
         cb(response, error);
     });	
 }
 
-const completeTurn = (cb, gameId) => {
+export const completeTurn = (cb, gameId) => {
     client({method: 'POST', path: '/api/completeTurn', params: {gameId}}).done((response, error) => {
         cb(response, error);
     });	   
 }
 
-const getPlayersByName = (cb, name) => {
+export const getPlayersByName = (cb, name) => {
     client({method: 'GET', path: '/api/playersbyname', params: {name}}).done((response, error) => {
         cb(response, error);
     });	
 }
 
-const deleteSeek = (cb, id) => {
+export const deleteSeek = (cb, id) => {
     client({method: 'DELETE', path: '/api/seek', params: {id}}).done((response, error) => {
         cb(response, error);
     });	      
 }
-
-export { getSeeks, getSeek, getSeeksBySeekerId, deleteSeeksBySeekerId, postSeek, postAccept, getGame, postMove, completeTurn, getPlayersByName, deleteSeek };
