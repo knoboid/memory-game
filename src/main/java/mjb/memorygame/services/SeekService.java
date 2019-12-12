f13628e5197da167c27172481bd5c703e0a867de
MJ Bingley <kn_b_y@yahoo.co.uk>
2019-12-12 13:51:16 +0000

    Removed the service interfaces.

--- src/main/java/mjb/memorygame/services/SeekService.java
+++ src/main/java/mjb/memorygame/services/SeekService.java
@@ -1,11 +1,46 @@
 package mjb.memorygame.services;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import mjb.memorygame.entities.Seek;
+import mjb.memorygame.events.SeeksEventHandler;
+import mjb.memorygame.repositories.SeekRepository;
 
-public interface SeekService {
+@Service
+public class SeekService {
 
-    public boolean isSeekAccepted(Seek seek);
+    private final static int SEEKS_KEEP_ALIVE_MINUTES = 20;
 
-    public void removeOldSeeks();
+    @Autowired
+    SeekRepository seeksRepository;
+
+    @Autowired
+	private SeeksEventHandler seeksEventHandler;
+
+    public boolean isSeekAccepted(Seek seek) {
+        return seek.getSeeker() != null && seek.getAccepter() != null;
+    }
+
+    public void removeOldSeeks() {
+        boolean wereSeeksDeleted = false;
+        List<Seek> seeks = seeksRepository.findAll();
+        Date now = new Date();
+        long id = -1;
+        for(Seek seek : seeks) {
+            long ageInMilis = now.getTime() - seek.getCreatedAt().getTime();
+            if (ageInMilis > SEEKS_KEEP_ALIVE_MINUTES * 60 * 1000) {
+                id = seek.id;
+                seeksRepository.deleteById(id);
+                wereSeeksDeleted = true;
+            }
+        }
+        if (wereSeeksDeleted) {
+            seeksEventHandler.deleteSeek(id);
+        }
+    }
 
 }
